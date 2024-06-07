a4b19688a2561f3f58a6fe282da84022f4a2593a
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-06 23:30:26 -0300

    create UserRepository

---
+++ Alertfly.App.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Alertfly.App.Core.Entities;
+
+namespace Alertfly.App.Core.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id);
+        Task<List<User>> GetAllAsync(Guid id);
+        Task<User> AddAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<User> DeleteByIdAsync(User user);
+    }
+}
---
+++ Alertfly.App.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alertfly.App.Infrastructure.Persistence.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        public async void DeleteByIdAsync(User user)
+        {
+            _context.Users.Remove(user);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public Task<List<User>> GetAllAsync(Guid id) => _context.Users.ToListAsync();
+
+        public Task<User?> GetByIdAsync(Guid id) => _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<User> UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
