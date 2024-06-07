d39554d90c7e7db3ddea5ce611ca2002f70144ee
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-07 11:05:41 -0300

    create IUserFlightRepository

---
+++ Alertfly.App.Core/Interfaces/Repositories/IUserFlightRepository.cs
@@ -0,0 +1,14 @@
+﻿using Alertfly.App.Core.Entities;
+
+namespace Alertfly.App.Core.Interfaces.Repositories
+{
+    public interface IUserFlightRepository
+    {
+
+        Task<UserFlight?> GetByIdAsync(Guid id);
+        Task<List<UserFlight>?> GetAllAsync();
+        Task AddAsync(UserFlight userFlight);
+        Task UpdateAsync(UserFlight userFlight);
+        Task DeleteAsync(UserFlight userFlight);
+    }
+}
---
+++ Alertfly.App.Infrastructure/Persistence/Repositories/UserFlightRepository.cs
@@ -0,0 +1,42 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alertfly.App.Infrastructure.Persistence.Repositories
+{
+    public class UserFlightRepository : IUserFlightRepository
+    {
+        private readonly AppDbContext _context;
+        public UserFlightRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserFlight?> GetByIdAsync(Guid id) => await _context.UserFlights.FirstOrDefaultAsync(x => x.Id == id);
+        public async Task<List<UserFlight>?> GetAllAsync() => await _context.UserFlights.ToListAsync();
+
+      
+        //BindFlightToUser
+        public async Task AddAsync(UserFlight userFlight)
+        {
+            await _context.AddAsync(userFlight);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(UserFlight userFlight)
+        {
+            _context.Remove(userFlight);
+            
+            await _context.SaveChangesAsync();
+
+        }
+        public async Task UpdateAsync(UserFlight userFlight)
+        {
+            _context.UserFlights.Update(userFlight);
+
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
