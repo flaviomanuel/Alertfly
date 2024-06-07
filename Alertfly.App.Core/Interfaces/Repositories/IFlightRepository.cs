8d821b7d504beb13a2236af130525277de025ed9
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-07 00:15:55 -0300

    create FlightRepository

---
+++ Alertfly.App.Core/Interfaces/Repositories/IFlightRepository.cs
@@ -0,0 +1,13 @@
+﻿using Alertfly.App.Core.Entities;
+
+namespace Alertfly.App.Core.Interfaces.Repositories
+{
+    public interface IFlightRepository
+    {
+        Task<Flight?> GetByIdAsync(Guid id);
+        Task<List<Flight>> GetAllAsync(Guid id);
+        Task<Flight> AddAsync(Flight flight);
+        Task<Flight> UpdateAsync(Flight flight);
+        Task DeleteByIdAsync(Flight flight);
+    }
+}
---
+++ Alertfly.App.Infrastructure/Persistence/Repositories/FlightRepository.cs
@@ -0,0 +1,41 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alertfly.App.Infrastructure.Persistence.Repositories
+{
+    public class FlightRepository : IFlightRepository
+    {
+        private readonly AppDbContext _context;
+        public FlightRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<List<Flight>> GetAllAsync(Guid id) => _context.Flights.ToListAsync();
+        public Task<Flight?> GetByIdAsync(Guid id) => _context.Flights.FirstOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Flight> AddAsync(Flight flight)
+        {
+            await _context.Flights.AddAsync(flight);
+
+            await _context.SaveChangesAsync();
+
+            return flight;
+        }
+        public async Task<Flight> UpdateAsync(Flight flight)
+        {
+            _context.Flights.Update(flight);
+
+            await _context.SaveChangesAsync();
+
+            return flight;
+        }
+        public async Task DeleteByIdAsync(Flight flight)
+        {
+            _context.Flights.Remove(flight);
+
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
