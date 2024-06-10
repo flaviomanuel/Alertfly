2679251d96bd02211dc6aac8c826d5de07185e73
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-10 14:43:14 -0300

    create UpdateUserCommand

---
+++ Alertfly.App.Application/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateUser
+{
+    public class UpdateUserCommand : IRequest<Unit>
+    {
+        public UpdateUserCommand(Guid id, string name, string email, string phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateUser
+{
+    public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var userFound = await _userRepository.GetByIdAsync(request.Id);
+
+            if (userFound is null) throw new Exception("User not Found");
+
+            var newUser = new User(request.Name, request.Email, request.PhoneNumber);
+
+            await _userRepository.UpdateAsync(newUser);
+
+            return Unit.Value;
+        }
+    }
+}
