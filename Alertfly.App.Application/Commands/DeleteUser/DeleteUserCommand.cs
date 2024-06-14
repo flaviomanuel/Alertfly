0736fcd4f3c72caeeb069ae140bfa4fbf6111568
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-13 23:46:44 -0300

    create DeleteUserCommand

---
+++ Alertfly.App.Application/Commands/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.DeleteUser
+{
+    public class DeleteUserCommand : IRequest<Unit>
+    {
+        public DeleteUserCommand(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Unit>
+    {
+        private readonly IUserRepository _userRepository;
+        public DeleteUserCommandHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if (user is null) throw new Exception("User not Found");
+
+            await _userRepository.DeleteByIdAsync(user);
+
+            return Unit.Value;
+        }
+    }
+}
