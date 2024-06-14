119c094ffb20e66342fae34d73d165b8a6170758
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 17:50:54 -0300

    create DeleteUserFlightCommand

---
+++ Alertfly.App.Application/Commands/DeleteUserFlight/DeleteUserFlightCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.DeleteUserFlight
+{
+    public class DeleteUserFlightCommand : IRequest<Unit>
+    {
+        public DeleteUserFlightCommand(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; private set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/DeleteUserFlight/DeleteUserFlightCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.DeleteUserFlight
+{
+    public class DeleteUserFlightCommandHandler : IRequestHandler<DeleteUserFlightCommand, Unit>
+    {
+        private readonly IUserFlightRepository _userFlightRepository;
+
+        public DeleteUserFlightCommandHandler(IUserFlightRepository userFlightRepository)
+        {
+            _userFlightRepository = userFlightRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteUserFlightCommand request, CancellationToken cancellationToken)
+        {
+            var userFlight = await _userFlightRepository.GetByIdAsync(request.Id);
+
+            if (userFlight == null) throw new Exception("UserFlight Not Found");
+
+            await _userFlightRepository.DeleteAsync(userFlight);
+
+            return Unit.Value;
+        }
+    }
+}
