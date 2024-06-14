c7d1654f88f94b928c24bf63cebf261a7a66a780
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 01:11:35 -0300

    create UpdateFlightCommand

---
+++ Alertfly.App.Application/Commands/UpdateFlight/UpdateFlightCommand.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateFlight
+{
+    public class UpdateFlightCommand : IRequest<Unit>
+    {
+        public UpdateFlightCommand(Guid id, string title, string origin, string destiny, DateTime flightDate)
+        {
+            Id = id;
+            Title = title;
+            Origin = origin;
+            Destiny = destiny;
+            FlightDate = flightDate;
+        }
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Origin { get; private set; }
+        public string Destiny { get; private set; }
+        public DateTime FlightDate { get; private set; }
+
+    }
+}
---
+++ Alertfly.App.Application/Commands/UpdateFlight/UpdateFlightCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateFlight
+{
+    public class UpdateFlightCommandHandler : IRequestHandler<UpdateFlightCommand, Unit>
+    {
+        private readonly IFlightRepository _flightRepository;
+
+        public UpdateFlightCommandHandler(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateFlightCommand request, CancellationToken cancellationToken)
+        {
+            var flight = await _flightRepository.GetByIdAsync(request.Id);
+
+            if (flight is null) throw new Exception("Flight Not Found!");
+
+            flight.Update(request.Title, request.Origin, request.Destiny, request.FlightDate);
+
+            await _flightRepository.UpdateAsync(flight);
+
+            return Unit.Value;
+        }
+    }
+}
