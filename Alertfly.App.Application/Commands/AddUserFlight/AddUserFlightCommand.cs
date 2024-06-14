5a6f5e9d0ca54ede2b7c18411b347454bb094ba9
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 16:14:41 -0300

    create AddUserFlightCommand

---
+++ Alertfly.App.Application/Commands/AddUserFlight/AddUserFlightCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.AddUserFlight
+{
+    public class AddUserFlightCommand : IRequest<Unit>
+    {
+        public AddUserFlightCommand(Guid userId, Guid flightId, DateTime alertAt)
+        {
+
+            UserId = userId;
+            FlightId = flightId;
+            AlertAt = alertAt;
+            CreatedAt = DateTime.Now;
+        }
+
+        public DateTime AlertAt { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid FlightId { get; private set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/AddUserFlight/AddUserFlightCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.AddUserFlight
+{
+    internal class AddUserFlightCommandHandler : IRequestHandler<AddUserFlightCommand, Unit>
+    {
+        private readonly IUserFlightRepository _userFlightRepository;
+
+        public AddUserFlightCommandHandler(IUserFlightRepository userFlightRepository)
+        {
+            _userFlightRepository = userFlightRepository;
+        }
+
+        public async Task<Unit> Handle(AddUserFlightCommand request, CancellationToken cancellationToken)
+        {
+            var userFlight = new UserFlight(request.UserId, request.FlightId, request.AlertAt);
+
+            await _userFlightRepository.AddAsync(userFlight);
+
+            return Unit.Value;
+        }
+    }
+}
