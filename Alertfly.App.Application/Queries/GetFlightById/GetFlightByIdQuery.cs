2ae979bae593b5faed10dc75ba6bd775a229f470
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 11:35:06 -0300

    Create GetFlightByIdQuery

---
+++ Alertfly.App.Application/Queries/GetFlightById/GetFlightByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using Alertfly.App.Core.Entities;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetFlightById
+{
+    public class GetFlightByIdQuery : IRequest<Flight>
+    {
+        public GetFlightByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; private set; }
+    }
+}
---
+++ Alertfly.App.Application/Queries/GetFlightById/GetFlightByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.App.Application.Queries.GetFlightById
+{
+    public class GetFlightByIdQueryHandler : IRequestHandler<GetFlightByIdQuery, Flight>
+    {
+        private readonly IFlightRepository _flightRepository;
+
+        public GetFlightByIdQueryHandler(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<Flight> Handle(GetFlightByIdQuery request, CancellationToken cancellationToken)
+        {
+            var flight = await _flightRepository.GetByIdAsync(request.Id);
+
+            if (flight is null) throw new Exception("Flight Not Found");
+
+            return flight;
+        }
+    }
+}
