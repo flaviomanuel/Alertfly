8d34ef123f6057296d0c021a56a9adf3f3eddde9
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 19:23:34 -0300

    return UserFlightViewModel in  GetAllUserFlightsQuery

--- Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQuery.cs
+++ Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQuery.cs
@@ -1,9 +1,10 @@
-﻿using Alertfly.App.Core.Entities;
+﻿using Alertfly.App.Application.ViewModels;
+using Alertfly.App.Core.Entities;
 using MediatR;
 
 namespace Alertfly.App.Application.Queries.GetAllUserFlights
 {
-    public class GetAllUserFlightsQuery : IRequest<List<UserFlight>?>
+    public class GetAllUserFlightsQuery : IRequest<List<UserFlightViewModel>?>
     {
     }
 }
--- Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQueryHandler.cs
+++ Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQueryHandler.cs
@@ -1,10 +1,11 @@
-﻿using Alertfly.App.Core.Entities;
+﻿using Alertfly.App.Application.ViewModels;
+using Alertfly.App.Core.Entities;
 using Alertfly.App.Core.Interfaces.Repositories;
 using MediatR;
 
 namespace Alertfly.App.Application.Queries.GetAllUserFlights
 {
-    public class GetAllUserFlightsQueryHandler : IRequestHandler<GetAllUserFlightsQuery, List<UserFlight>?>
+    public class GetAllUserFlightsQueryHandler : IRequestHandler<GetAllUserFlightsQuery, List<UserFlightViewModel>?>
     {
         private readonly IUserFlightRepository _userFlightRepository;
 
@@ -13,11 +14,17 @@ namespace Alertfly.App.Application.Queries.GetAllUserFlights
             _userFlightRepository = userFlightRepository;
         }
 
-        public async Task<List<UserFlight>?> Handle(GetAllUserFlightsQuery request, CancellationToken cancellationToken)
+        public async Task<List<UserFlightViewModel>?> Handle(GetAllUserFlightsQuery request, CancellationToken cancellationToken)
         {
             var userFlights = await _userFlightRepository.GetAllAsync();
 
-            return userFlights;
+            if (userFlights == null) return null;
+
+            var userFlightsViewModels = userFlights
+                .Select(x => new UserFlightViewModel(x.Id, x.UserId, x.FlightId, x.AlertAt, x.CreatedAt))
+                .ToList();
+
+            return userFlightsViewModels;
         }
     }
 }
