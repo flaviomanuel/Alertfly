24e00f0094b9de521e7e3ccfcb9608a00034245f
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 18:40:09 -0300

    create GetAllUserFlightsQuery

---
+++ Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Alertfly.App.Core.Entities;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetAllUserFlights
+{
+    public class GetAllUserFlightsQuery : IRequest<List<UserFlight>?>
+    {
+    }
+}
---
+++ Alertfly.App.Application/Queries/GetAllUserFlights/GetAllUserFlightsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetAllUserFlights
+{
+    public class GetAllUserFlightsQueryHandler : IRequestHandler<GetAllUserFlightsQuery, List<UserFlight>?>
+    {
+        private readonly IUserFlightRepository _userFlightRepository;
+
+        public GetAllUserFlightsQueryHandler(IUserFlightRepository userFlightRepository)
+        {
+            _userFlightRepository = userFlightRepository;
+        }
+
+        public async Task<List<UserFlight>?> Handle(GetAllUserFlightsQuery request, CancellationToken cancellationToken)
+        {
+            var userFlights = await _userFlightRepository.GetAllAsync();
+
+            return userFlights;
+        }
+    }
+}
