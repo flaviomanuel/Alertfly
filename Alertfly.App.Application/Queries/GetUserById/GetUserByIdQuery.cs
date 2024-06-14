0d463d9ca266c95d98efbd2cd03f43841d978fbd
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 00:16:37 -0300

    create GetUserByIdQuery

---
+++ Alertfly.App.Application/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Alertfly.App.Core.Entities;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetUserById
+{
+    public class GetUserByIdQuery : IRequest<User>
+    {
+        public GetUserByIdQuery(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+    }
+}
---
+++ Alertfly.App.Application/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetUserById
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if (user is null) throw new Exception("User not Found");
+
+            return user;
+        }
+    }
+}
