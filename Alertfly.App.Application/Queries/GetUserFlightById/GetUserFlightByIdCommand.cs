36d5bfecae519c4f6436cbb15c81bc67fd0060b5
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 18:07:29 -0300

    create GetUserFlightByIdCommand

---
+++ Alertfly.App.Application/Queries/GetUserFlightById/GetUserFlightByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using Alertfly.App.Core.Entities;
+using MediatR;
+
+namespace Alertfly.App.Application.Queries.GetUserFlightById
+{
+    public class GetUserFlightByIdCommand : IRequest<UserFlight?>
+    {
+        public GetUserFlightByIdCommand(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Alertfly.App.Application/Queries/GetUserFlightById/GetUserFlightByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.App.Application.Queries.GetUserFlightById
+{
+    public class GetUserFlightByIdCommandHandler : IRequestHandler<GetUserFlightByIdCommand, UserFlight?>
+    {
+        private readonly IUserFlightRepository _userFlightRepository;
+
+        public GetUserFlightByIdCommandHandler(IUserFlightRepository userFlightRepository)
+        {
+            _userFlightRepository = userFlightRepository;
+        }
+
+        public async Task<UserFlight?> Handle(GetUserFlightByIdCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userFlightRepository.GetByIdAsync(request.Id);
+
+            return user;
+        }
+    }
+}
