c3a1d361b162da8e2e3458c77e85afe14677c325
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 11:09:21 -0300

    create DeleteFlightCommand

---
+++ Alertfly.App.Application/Commands/DeleteFlight/DeleteFlightCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.DeleteFlight
+{
+    public class DeleteFlightCommand : IRequest<Unit>
+    {
+        public DeleteFlightCommand(Guid id)
+        {
+            Id = id;
+        }
+        public Guid Id { get; private set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/DeleteFlight/DeleteFlightCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.App.Application.Commands.DeleteFlight
+{
+    internal class DeleteFlightCommandHandler : IRequestHandler<DeleteFlightCommand, Unit>
+    {
+        private readonly IFlightRepository _flightRepository;
+
+        public DeleteFlightCommandHandler(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<Unit> Handle(DeleteFlightCommand request, CancellationToken cancellationToken)
+        {
+            var flight = await _flightRepository.GetByIdAsync(request.Id);
+
+            if (flight is null) throw new Exception("Flight Not Found");
+
+            await _flightRepository.DeleteAsync(flight);
+
+            return Unit.Value;
+        }
+    }
+}
