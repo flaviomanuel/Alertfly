44c9bb996b694f274f566d23ce649fb55b6ad5b7
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 00:33:59 -0300

    create AddFlightCommand

---
+++ Alertfly.App.Application/Commands/AddFlight/AddFlightCommand.cs
@@ -0,0 +1,25 @@
+﻿using Alertfly.App.Core.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.App.Application.Commands.AddFlight
+{
+    public class AddFlightCommand : IRequest<Unit>
+    {
+        public AddFlightCommand(string title, string origin, string destiny, DateTime flightDate)
+        {  
+            Title = title;
+            Origin = origin;
+            Destiny = destiny;
+            FlightDate = flightDate;
+        }
+        public string Title { get; private set; }
+        public string Origin { get; private set; }
+        public string Destiny { get; private set; }
+        public DateTime FlightDate { get; private set; }
+    }
+}
---
+++ Alertfly.App.Application/Commands/AddFlight/AddFlightCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.App.Application.Commands.AddFlight
+{
+    public class AddFlightCommandHandler : IRequestHandler<AddFlightCommand, Unit>
+    {
+        private readonly IFlightRepository _flightRepository;
+
+        public AddFlightCommandHandler(IFlightRepository flightRepository)
+        {
+            _flightRepository = flightRepository;
+        }
+
+        public async Task<Unit> Handle(AddFlightCommand request, CancellationToken cancellationToken)
+        {
+            var flight = new Flight(request.Title, request.Origin,request.Destiny, request.FlightDate);
+
+            await _flightRepository.AddAsync(flight);
+
+            return Unit.Value;
+        }
+    }
+}
