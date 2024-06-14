b8ad9d61bb1ec87afa27366c8720feff115f0241
Flávio Manuel <flaviomanuel258@gmail.com>
2024-06-14 17:24:27 -0300

    create UpdateUserAlertAtCommandHandler

---
+++ Alertfly.App.Application/Commands/UpdateUserAlertAt/UpdateUserAlertAtCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateUserAlertAt
+{
+    public class UpdateUserAlertAtCommand : IRequest<Unit>
+    {
+        public UpdateUserAlertAtCommand(Guid id, DateTime alertAt)
+        {
+            Id = id;
+            AlertAt = alertAt;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime AlertAt { get; private set; }
+     
+    }
+}
---
+++ Alertfly.App.Application/Commands/UpdateUserAlertAt/UpdateUserAlertAtCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using Alertfly.App.Core.Interfaces.Repositories;
+using MediatR;
+
+namespace Alertfly.App.Application.Commands.UpdateUserAlertAt
+{
+    public class UpdateUserAlertAtCommandHandler : IRequestHandler<UpdateUserAlertAtCommand, Unit>
+    {
+        private readonly IUserFlightRepository _userFlightRepository;
+
+        public UpdateUserAlertAtCommandHandler(IUserFlightRepository userFlightRepository)
+        {
+            _userFlightRepository = userFlightRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateUserAlertAtCommand request, CancellationToken cancellationToken)
+        {
+            var userFlight = await _userFlightRepository.GetByIdAsync(request.Id);
+
+            if (userFlight == null) throw new Exception("UserFlight Not Found!");
+
+
+            userFlight.UpdateAlertAt(request.AlertAt);
+
+            await _userFlightRepository.UpdateAsync(userFlight);
+
+            return Unit.Value;
+        }
+    }
+}
