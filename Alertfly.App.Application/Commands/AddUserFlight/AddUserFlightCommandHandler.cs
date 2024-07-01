1c0bac6e46f06750e5fb867e7fb6d01b96122e75
Flávio Manuel <flaviomanuel258@gmail.com>
2024-07-01 14:17:19 -0300

    publishing message to queue AlertFlight when AddUserFlight

--- Alertfly.App.Application/Commands/AddUserFlight/AddUserFlightCommandHandler.cs
+++ Alertfly.App.Application/Commands/AddUserFlight/AddUserFlightCommandHandler.cs
@@ -1,16 +1,25 @@
-﻿using Alertfly.App.Core.Entities;
+﻿using Alertfly.App.Application.ViewModels;
+using Alertfly.App.Core.Entities;
+using Alertfly.App.Core.Interfaces;
 using Alertfly.App.Core.Interfaces.Repositories;
 using MediatR;
+using System.Text;
+using System.Text.Json;
 
 namespace Alertfly.App.Application.Commands.AddUserFlight
 {
     internal class AddUserFlightCommandHandler : IRequestHandler<AddUserFlightCommand, Unit>
     {
         private readonly IUserFlightRepository _userFlightRepository;
+        private readonly IMessageBusService _messageBusService;
+        private const string QUEUE_NAME = "AlertFlight";
+        private const string ROUTING_KEY = "created.AlertFlight";
 
-        public AddUserFlightCommandHandler(IUserFlightRepository userFlightRepository)
+
+        public AddUserFlightCommandHandler(IUserFlightRepository userFlightRepository, IMessageBusService messageBusService)
         {
             _userFlightRepository = userFlightRepository;
+            _messageBusService = messageBusService;
         }
 
         public async Task<Unit> Handle(AddUserFlightCommand request, CancellationToken cancellationToken)
@@ -18,6 +27,10 @@ namespace Alertfly.App.Application.Commands.AddUserFlight
             var userFlight = new UserFlight(request.UserId, request.FlightId, request.AlertAt);
 
             await _userFlightRepository.AddAsync(userFlight);
+
+            var userFlightViewModel = new UserFlightViewModel(userFlight.Id, userFlight.UserId, userFlight.FlightId, userFlight.AlertAt, userFlight.CreatedAt);
+
+            _messageBusService.Publish(userFlightViewModel, QUEUE_NAME, ROUTING_KEY, QUEUE_NAME);
 
             return Unit.Value;
         }
