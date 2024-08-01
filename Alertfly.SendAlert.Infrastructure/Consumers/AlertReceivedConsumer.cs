21bbe0c165910803e3fd0830d41d4c08b5600df3
Flávio Manuel <flaviomanuel258@gmail.com>
2024-08-01 12:52:55 -0300

    initial create for Consumer AlertReceived

---
+++ Alertfly.SendAlert.Infrastructure/Consumers/AlertReceivedConsumer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+
+namespace Alertfly.SendAlert.Infrastructure.Consumers
+{
+    public class AlertReceivedConsumer : BackgroundService
+    {
+        private const string QUEUE_NAME = "AlertFlight";
+        private const string ROUTING_KEY = "created.AlertFlight";
+
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly IServiceProvider _serviceProvider;
+
+        public AlertReceivedConsumer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+            };
+
+            _connection = factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(
+                queue: QUEUE_NAME,
+                durable: false,
+                autoDelete: true,
+                exclusive: false,
+                arguments: null
+            );
+
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Alertfly.SendAlert.Infrastructure/IntegrationEvents/ReceivedAlertFlightIntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alertfly.SendAlert.Infrastructure.IntegrationEvents
+{
+    public class ReceivedAlertFlightIntegrationEvent
+    {
+        public ReceivedAlertFlightIntegrationEvent(Guid id, Guid userId, Guid flightId, DateTime alertAt, DateTime createdAt)
+        {
+            Id = id;
+            UserId = userId;
+            FlightId = flightId;
+            AlertAt = alertAt;
+            CreatedAt = createdAt;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime AlertAt { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public Guid UserId { get; private set; }
+        public Guid FlightId { get; private set; }
+    }
+}
