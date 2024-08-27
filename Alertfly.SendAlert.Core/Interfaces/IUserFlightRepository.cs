3c858f7230107a1d7f6b6b061aa936cfc6bf89dc
Flávio Manuel <flaviomanuel258@gmail.com>
2024-08-27 16:28:23 -0300

    create IUserFlightRepository

---
+++ Alertfly.SendAlert.Core/Interfaces/IUserFlightRepository.cs
@@ -0,0 +1,10 @@
+﻿using Alertfly.SendAlert.Core.DTOs;
+
+namespace Alertfly.SendAlert.Core.Interfaces
+{
+    public interface IUserFlightRepository
+    {
+        Task<UserFlightDetailsDTO?> GetUserFlightDetailsById(Guid userId, Guid flightId);
+
+    }
+}
---
+++ Alertfly.SendAlert.Infrastructure/Repositories/UserFlightRepository.cs
@@ -0,0 +1,38 @@
+﻿using Alertfly.SendAlert.Core.DTOs;
+using Alertfly.SendAlert.Core.Interfaces;
+using Alertfly.SendAlert.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alertfly.SendAlert.Infrastructure.Repositories
+{
+    public class UserFlightRepository : IUserFlightRepository
+    {
+        private readonly AlertflyContext _context;
+
+        public UserFlightRepository(AlertflyContext context) => _context = context;
+
+        public async Task<UserFlightDetailsDTO?> GetUserFlightDetailsById(Guid userId, Guid flightId)
+        {
+
+            var userFlightDetailsDto = await _context.UserFlights
+                  .Include(uf => uf.User)
+                  .Include(uf => uf.Flight)
+                  .Select(uf => new UserFlightDetailsDTO
+                  {
+                      UserId = uf.UserId,
+                      FlightId = uf.FlightId,
+                      Name = uf.User.Name,
+                      Email = uf.User.Email,
+                      PhoneNumber = uf.User.PhoneNumber,
+                      FlightTitle = uf.Flight.Title,
+                      Origin = uf.Flight.Origin,
+                      Destiny = uf.Flight.Destiny,
+                      FlightDate = uf.Flight.FlightDate,
+                      CreatedAt = uf.CreatedAt,
+                      AlertAt = uf.AlertAt
+                  }).SingleOrDefaultAsync(x => x.UserId == userId && x.FlightId == flightId);
+
+            return userFlightDetailsDto;
+        }
+    }
+}
