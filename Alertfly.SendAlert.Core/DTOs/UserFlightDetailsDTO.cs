34cd3e5daa8607ca3463803a925e93488dba3ab6
Flávio Manuel <flaviomanuel258@gmail.com>
2024-08-27 16:28:01 -0300

    Create UserFlightDetailsDTO.cs

---
+++ Alertfly.SendAlert.Core/DTOs/UserFlightDetailsDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace Alertfly.SendAlert.Core.DTOs
+{
+    public class UserFlightDetailsDTO
+    {
+        public Guid UserId { get; set; }
+        public Guid FlightId { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string FlightTitle { get; set; }
+        public string Origin { get; set; }
+        public string Destiny { get; set; }
+        public DateTime FlightDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime AlertAt { get; set; }
+
+    }
+}
