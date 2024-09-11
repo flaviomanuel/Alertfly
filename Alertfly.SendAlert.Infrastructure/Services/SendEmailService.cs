5d1401ea2b57a6caeb53c64ce44b882fc3950b86
Flávio Manuel <flaviomanuel258@gmail.com>
2024-09-11 09:58:21 -0300

    (Api.SendAlert): use environment variables in SendEmailService to credentials

--- Alertfly.SendAlert.Infrastructure/Services/SendEmailService.cs
+++ Alertfly.SendAlert.Infrastructure/Services/SendEmailService.cs
@@ -1,4 +1,5 @@
 ﻿using Alertfly.SendAlert.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
 using System.Net;
 using System.Net.Mail;
 
@@ -6,16 +7,38 @@ namespace Alertfly.SendAlert.Infrastructure.Services
 {
     public class SendEmailService : ISendEmailService
     {
-     
+        private string EMAIL_ADDRESS;
+        private string PASSWORD;
+        private string HOST;
+        private int PORT;
+
+        private readonly IConfiguration _configuration;
+
+        public SendEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            EMAIL_ADDRESS = _configuration["EmailServiceInfos:EmailAdress"];
+            PASSWORD = _configuration["EmailServiceInfos:Password"];
+            HOST = _configuration["EmailServiceInfos:Host"];
+            PORT = int.Parse(_configuration["EmailServiceInfos:Port"]);
+        }
+
         public void SendEmail(string to, string subject, string body)
         {
             using SmtpClient smtpClient = new SmtpClient();
 
-            smtpClient.Host = "localhost";
+            smtpClient.Host = HOST;
+            smtpClient.Port = PORT;
             smtpClient.EnableSsl = true;
-            smtpClient.Credentials = new NetworkCredential("usuario", "senha");
+            smtpClient.Credentials = new NetworkCredential(EMAIL_ADDRESS, PASSWORD);
 
-            var mailMessage = CreateMailMessage("meuEmail", to, subject, body);
+            using var mailMessage = new MailMessage();
+
+            mailMessage.From = new MailAddress(EMAIL_ADDRESS);
+            mailMessage.To.Add(new MailAddress(to));
+            mailMessage.Subject = subject;
+            mailMessage.Body = body;
 
             try
             {
@@ -27,16 +50,6 @@ namespace Alertfly.SendAlert.Infrastructure.Services
             }
         }
     
-        private MailMessage CreateMailMessage(string from, string to, string subject, string body) 
-        {
-            using MailMessage mailMessage = new MailMessage();
 
-            mailMessage.From = new MailAddress(from);
-            mailMessage.To.Add(new MailAddress(to));
-            mailMessage.Subject = subject;
-            mailMessage.Body = body;
-
-            return mailMessage;
-        }
     }
 }
